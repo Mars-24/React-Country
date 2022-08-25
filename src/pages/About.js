83253b5cfb07150e14bacfddf6035db63eb94597
Mars-24 <condormatt6@gmail.com>
2022-08-25 00:08:16 +0000

    Country map with react

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A Propos</h1>
+            <br />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat non distinctio libero possimus aperiam qui tenetur ipsum saepe minus iusto facere repellendus nulla, amet sint dolore laboriosam, consectetur eum magnam temporibus minima dolorum fugit numquam accusantium. Exercitationem optio, eaque, incidunt eum quo tempore cumque ea tenetur at rem dolore id accusamus placeat nihil mollitia voluptates quis perferendis consectetur. Aperiam omnis corporis modi vero molestias? Repudiandae adipisci, eos voluptates quia quod ipsam molestiae commodi consectetur placeat quibusdam incidunt architecto, aspernatur laboriosam dolor libero expedita. Iure aperiam molestias facere nisi facilis repellat id, eligendi perspiciatis, inventore debitis asperiores laborum quaerat laboriosam nulla?</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod quaerat eaque saepe minus molestias veritatis repellat fuga illum facilis? Aspernatur delectus blanditiis dolores officia, dolore nisi ex id natus ipsa! Eaque ea odio doloribus, qui saepe maxime officiis. Quo dignissimos cumque repellendus quos, laudantium, nobis accusamus sed id cum minus maxime architecto. Ullam cupiditate qui non perferendis quo nobis delectus inventore amet quos voluptatum, repellendus earum laborum animi? Eligendi delectus harum ab, minima dolorem beatae asperiores eius sit voluptate explicabo necessitatibus sequi ducimus reprehenderit omnis accusantium perspiciatis, neque officiis maiores suscipit porro recusandae. Id, tenetur molestiae! Cumque voluptatum expedita et nostrum eius maxime adipisci error cupiditate suscipit rerum. Facere doloremque deleniti assumenda velit odit quibusdam veritatis fugiat esse rem aspernatur architecto, culpa, nesciunt ullam ab minus dolorum. Earum, voluptatum repellat! Cupiditate expedita odit, necessitatibus quibusdam quos, totam accusamus libero id impedit harum, at sint doloribus incidunt quo dignissimos cumque voluptas dicta. Ipsam itaque nulla officiis obcaecati aliquam illo minus tempora perferendis, doloribus magni debitis, modi repellat, aspernatur hic animi maxime ea et voluptates molestias quia nobis tenetur atque quisquam placeat? Sed ipsam illum nobis porro commodi mollitia quam eaque magnam consequatur in assumenda enim, architecto harum pariatur tempora odio cupiditate!</p>
+        </div>
+    );
+};
+
+export default About;
